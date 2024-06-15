67d1ce1edfa2165e1dbc149e99f93fe0941b968c
anatolii.utkin <anatoly.ytkin@gmail.com>
2024-06-15 21:37:44 +0400

    updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,53 +1,35 @@
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.Instant
+import java.time.LocalDateTime
 import java.util.concurrent.ConcurrentHashMap
 
+const val file = "C:\\Users\\lifte\\Downloads\\ip_addresses\\ip_addresses"
+
 fun main()  {
+     println("${LocalDateTime.now()} started")
      val start = Instant.now().epochSecond
-     val file = "C:\\Users\\lifte\\Downloads\\ip_addresses\\ip_addresses"
-     val stream = Files.lines(Path.of(file))
 
-//     val map = HashMap<Int, String>(16_582_000)
-     val firstOctets = ConcurrentHashMap<Int, Int>()
+     val firstOctets = ConcurrentHashMap<String, Int>()
+     firstOctets.put("1.", 0)
+     firstOctets.put("0.", 0)
+//          getFirstOctetsMap()
 
-//     launch {
-          stream.use {
-               it.parallel().forEach { x ->
-                    val firstOctetOfIp = x.split(".").get(0).toInt()
-                    firstOctets.put(firstOctetOfIp, 0)
-               }
-          }
-//     }
-//     while(firstOctets.isEmpty()) {
-//          Thread.sleep(2000)
-//     }
-     println("Size of firstOctets ${firstOctets.size}")
-     val map = ConcurrentHashMap<Int, MutableSet<String>>(firstOctets.size)
+//     println("${LocalDateTime.now()} Size of firstOctets ${firstOctets.size}")
      for(i in firstOctets.keys()) {
           val start = Instant.now().epochSecond
+          val hashSet = ConcurrentHashMap.newKeySet<String>(4_000_000)
           val stream = Files.lines(Path.of(file))
           stream.use {
                it.parallel().forEach { x ->
-                    val firstOctetOfIp = x.split(".").get(0).toInt()
-                    if(i == firstOctetOfIp) {
-                         map.compute(firstOctetOfIp) {_, v ->
-                              if(v == null) {
-                                   return@compute mutableSetOf<String>(x)
-                              } else {
-                                   v.add(x)
-                                   return@compute v
-                              }
-                         }
+                    if(x.startsWith(i)) {
+                         hashSet.add(x)
                     }
                }
           }
-          val countOfIp = map.get(i)?.count()
-          if (countOfIp != null) {
-               firstOctets.put(i, countOfIp)
-               map.remove(i)
-          }
-          println("Count of ip with first octet $i is $countOfIp")
+          val value = hashSet.count()
+          firstOctets.put(i, value)
+          println("Count of ip with first octet $i is $value")
           val end = Instant.now().epochSecond
           println("Time spent on cycle ${(end - start) / 60}")
      }
@@ -57,4 +39,18 @@ fun main()  {
      val time = (end - start) / 60
      println("Size of unique ips: $uniqueIps  lines. Minutes spent: $time")
 
+}
+
+private fun getFirstOctetsMap(): ConcurrentHashMap<String, Int> {
+     val stream = Files.lines(Path.of(file))
+
+     val firstOctets = ConcurrentHashMap<String, Int>()
+
+     stream.use {
+          it.parallel().forEach { x ->
+               val firstOctetOfIp = x.split(".").get(0)
+               firstOctets.put("$firstOctetOfIp.", 0)
+          }
+     }
+     return firstOctets
 }
