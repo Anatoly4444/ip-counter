6cc3e95ea6e959ee00e3ade7fa8c5b54a11b8835
anatolii.utkin <anatoly.ytkin@gmail.com>
2024-06-16 19:43:07 +0400

    refactored

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,9 +10,9 @@ import kotlin.time.measureTime
 
 const val file = "C:\\Users\\lifte\\Downloads\\ip_addresses\\ip_addresses"
 val errorCounter = AtomicInteger(0)
-val dispatcher = Executors.newFixedThreadPool(12).asCoroutineDispatcher()
+val dispatcher = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
 
-fun main() = runBlocking {
+fun main() {
      println("${LocalDateTime.now()} started")
 
      var firstOctets: ConcurrentHashMap<String, Int>
@@ -21,41 +21,42 @@ fun main() = runBlocking {
      }
      println("First octets found in ${time.inWholeMinutes} minutes. size ${firstOctets.size}")
 
-     val measureTime = measureTime {
-          val count = firstOctets.map { item ->
-               async(dispatcher) {
-                    try {
-                         return@async countAddresses(item.key)
-                    } catch (e: Exception) {
-                         println("Exception occurred $e")
-                         return@async retry { countAddresses(item.key) }
-                    }
-               }
-          }.awaitAll()
-               .sum()
-          println("Amount of unique addresses is $count")
-          println("Errors occurred $errorCounter ")
+     runBlocking {
+          val measureTime = measureTime {
+               val count = firstOctets.map {  countAsync(it) }.awaitAll().sum()
+               println("Amount of unique addresses is $count")
+               println("Errors occurred $errorCounter ")
+          }
+          println("${LocalDateTime.now()} Found in ${measureTime.inWholeMinutes} minutes")
      }
-     println("${LocalDateTime.now()} Found in ${measureTime.inWholeMinutes} minutes")
 }
+
+private fun CoroutineScope.countAsync(item: Map.Entry<String, Int>): Deferred<Int> =
+     async(dispatcher) {
+          try {
+               return@async countAddresses(item.key)
+          } catch (e: Exception) {
+               println("Exception occurred $e")
+               return@async retry { countAddresses(item.key) }
+          }
+     }
 
 private fun countAddresses( octet: String): Int {
      println("Octet $octet processing started")
-     val start = Instant.now().epochSecond
-
-     val hashSet = HashSet<String>(4_000_000)
-     val stream = Files.lines(Path.of(file))
-     stream.use {
-          it.sequential().forEach { x ->
-               if (x.startsWith(octet)) {
-                    hashSet.add(x)
+     var value: Int
+     val measureTime = measureTime {
+          val hashSet = HashSet<String>(4_000_000)
+          val stream = Files.lines(Path.of(file))
+          stream.use {
+               it.sequential().forEach { x ->
+                    if (x.startsWith(octet)) {
+                         hashSet.add(x)
+                    }
                }
           }
+          value = hashSet.count()
      }
-     val value = hashSet.count()
-
-     val end = Instant.now().epochSecond
-     println("Count of ip with first octet $octet is $value. Spent  ${(end - start) / 60} minutes on cycle")
+     println("Count of octet $octet is $value. Spent $measureTime minutes")
      return value
 }
 
